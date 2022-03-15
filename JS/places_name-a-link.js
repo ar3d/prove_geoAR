1368acc03aa26b6cad9b5b8a59fb6219b560617b
ar3d <info@ar3d.it>
2022-03-15 09:08:14 +0100

    Update places_name-a-link.js

--- JS/places_name-a-link.js
+++ JS/places_name-a-link.js
@@ -96,6 +96,7 @@ function renderPlaces(places) {
         text.setAttribute('href', place.link);
         text.setAttribute('scale', '5 5 5');
 		text.setAttribute('image', 'assets/verde.jpg');
+		text.setAttribute('visualAspectEnabled', 'false');
 
         text.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded', { detail: { component: this.el }}))
